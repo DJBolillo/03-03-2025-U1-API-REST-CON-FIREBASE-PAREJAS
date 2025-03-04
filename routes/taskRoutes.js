78c9fd8cca38c3c06457f9fb314f34f51b20deab
YVES MANUEL PORTUGAL DE LA PAZ <160196519+DJBolillo@users.noreply.github.com>
2025-03-03 22:36:46 -0700

    Delete routes directory

--- routes/taskRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const taskController = require("../controllers/TaskController")
-router.get('/',taskController.getAllTasks);
-router.get('/:id',taskController.getTaskById);
-router.post('/',taskController.createTask);
-router.put('/:id',taskController.updateTask);
-
-module.exports=router;
